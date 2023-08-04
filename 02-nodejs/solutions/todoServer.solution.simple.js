b0a19cd9e9d36982bae96c655f28991d74fdea95
pratik2630 <pratikkorgaonkar26@gmail.com>
2023-08-04 23:48:02 +0530

    starting to do list app backend

--- 02-nodejs/solutions/todoServer.solution.simple.js
+++ 02-nodejs/solutions/todoServer.solution.simple.js
@@ -26,14 +26,7 @@ app.get('/todos', (req, res) => {
   res.json(todos);
 });
 
-app.get('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    res.json(todos[todoIndex]);
-  }
-});
+
 
 app.post('/todos', (req, res) => {
   const newTodo = {
@@ -45,16 +38,6 @@ app.post('/todos', (req, res) => {
   res.status(201).json(newTodo);
 });
 
-app.put('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos[todoIndex].title = req.body.title;
-    todos[todoIndex].description = req.body.description;
-    res.json(todos[todoIndex]);
-  }
-});
 
 app.delete('/todos/:id', (req, res) => {
   const todoIndex = findIndex(todos, parseInt(req.params.id));
@@ -71,4 +54,6 @@ app.use((req, res, next) => {
   res.status(404).send();
 });
 
+
+app.listen(3000)
 module.exports = app;
